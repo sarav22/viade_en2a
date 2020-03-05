c9c092cb4cf4cb23b33d6abba84850a8ff6005f0
Raúl Pérez Molinero <UO263743@uniovi.es>
2020-03-05 17:55:55 +0100

    Added 'See Routes' button to the Nav Bar

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -7,6 +7,12 @@ export const NavigationItems = [
     icon: '/img/icon/apps.svg',
     label: 'navBar.welcome',
     to: '/welcome'
+  },
+  {
+    id: 'seeRoutes',
+    icon: '/img/icon/map.svg',
+    label: 'navBar.seeRoutes',
+    to: '/welcome' //Specify here the address of the See Routes view
   }
 ];
 
