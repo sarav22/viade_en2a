fd530a60e4f6b00945a1b77291e3db779e10169b
Ángel Olmedo García <angelixus99@gmail.com>
2020-03-06 06:34:17 +0100

    Changed translation service export structure and added basic documentation

--- src/services/translator.js
+++ src/services/translator.js
@@ -1,13 +1,22 @@
-import Route from '../domain/domainClasses.js'
-import MapNode from '../domain/domainClasses.js'
-import Resource from '../domain/domainClasses.js'
-import Comment from '../domain/domainClasses.js'
+import Route from "../domain/domainClasses.js";
+import MapNode from "../domain/domainClasses.js";
+import Resource from "../domain/domainClasses.js";
+import Comment from "../domain/domainClasses.js";
 
-// TODO: Discuss if async makes sense here
-export const loadMapInfo = jsonUrl => {
+/**
+ * This service acts as an intermidiate step which connects the UI and the underlying POD server. It performs JSON-LD to Domain Model Objects translation and viceversa.
+ */
+export default class FormatTranslatorService {
+  // TODO: Discuss if async makes sense here
+  /**
+   * Loads route information from the POD and transforms it into Domain Model Objects usable on the user interface.
+   * @param {String} jsonURI URI where the route is stored on the POD
+   * @returns {Route}
+   */
+  loadMapInfo = async jsonURI => {
     // Load JSON-LD from map
 
-    var routeJson = /*Call underneath layer to retrieve JSON using the jsonUrl/name*/0;
+    var routeJson = /*Call underneath layer to retrieve JSON using the jsonUrl/name*/ 0;
 
     // Parse JSON-LD into MapNodes, Resources and Comments
 
@@ -15,16 +24,17 @@ export const loadMapInfo = jsonUrl => {
     var resourceList = [];
     var commentList = [];
 
-
     return new Route(mapNodeList, resourceList, commentList);
-};
+  };
 
-
-// TODO: Comment if async method makes sense
-export const sendMapInfo = async mapTriplet => {
-
+  // TODO: Comment if async method makes sense
+  
+  /**
+   * Translates Domain Model Objects into JSON-LD and stores it on the POD.
+   * @param {Route} route Route Domain Model Object representing a route
+   */
+  sendMapInfo = async route => {
     // Take MapTriplet info and create JSON-LD
-
-
     // Send JSON-LD to underneath layer calling the needed method
+  };
 }
