d56422e9e2f417b12e4bc96530359e7e8dbd9dd8
Ángel Olmedo García <angelixus99@gmail.com>
2020-03-09 14:11:32 +0100

    Finished JSON-LD to domain translator and updated domain classes

--- src/domain/domainClasses.js
+++ src/domain/domainClasses.js
@@ -1,8 +1,8 @@
 // TODO: Discuss if we should export every class or only the Route class.
 
-export default class MapNode {
-
-    constructor(latitude, longitude) {
+export default class TrackPoint {
+    constructor(name, latitude, longitude) {
+        this.name = name
         this.latitude = latitude;
         this.longitude = longitude;
     }
@@ -17,7 +17,9 @@ export default class Resource {
 
 // TODO: Discuss and create comment structure
 export default class Comment {
-
+    constructor(resourceUrl) {
+        this.resourceUrl = resourceUrl;
+    }
 }
 
 export default class Route {
@@ -25,6 +27,41 @@ export default class Route {
     constructor(mapNodelist, resourceList, commentList) {
         this.mapNodelist = mapNodelist;
         this.resourceList = resourceList;
-        this.commentList = commentList;
+        this.commentList = commentList ;
+    }
+
+
+}
+
+
+export default class RouteSpec {
+
+    /**
+     * You are supposed to pass a map with all the necessary information
+     * @param {*} params 
+     * 
+     * Note the ifs due to the possibility of ommiting data and provide them afeterwards
+     */
+    constructor(params) {
+
+        if (params.name){
+            this.name = params.name;
+        }
+
+        if (params.description){
+            this.description=params.description;
+        }
+
+        if (params.itineray){
+            this.itineray = params.itineray;
+        }
+
+        if (params.comments){
+            this.comments = params.comments;
+        }
+
+        if (params.resources){
+            this.resources = resources;
+        }
     }
 }
--- src/services/translator.js
+++ src/services/translator.js
@@ -1,40 +1,56 @@
-import Route from "../domain/domainClasses.js";
-import MapNode from "../domain/domainClasses.js";
-import Resource from "../domain/domainClasses.js";
-import Comment from "../domain/domainClasses.js";
+import {RouteSpec, Resource, Comment, TrackPoint} from '../domain/domainClasses.js'
 
-/**
- * This service acts as an intermidiate step which connects the UI and the underlying POD server. It performs JSON-LD to Domain Model Objects translation and viceversa.
- */
-export default class FormatTranslatorService {
-  // TODO: Discuss if async makes sense here
-  /**
-   * Loads route information from the POD and transforms it into Domain Model Objects usable on the user interface.
-   * @param {String} jsonURI URI where the route is stored on the POD
-   * @returns {Route}
-   */
-  loadMapInfo = async jsonURI => {
+// TODO: Discuss if async makes sense here
+export const loadMapInfo = jsonUrl => {
     // Load JSON-LD from map
 
-    var routeJson = /*Call underneath layer to retrieve JSON using the jsonUrl/name*/ 0;
+    var routeJson = /*Call underneath layer to retrieve JSON using the jsonUrl/name*/0;
 
-    // Parse JSON-LD into MapNodes, Resources and Comments
-
-    var mapNodeList = [];
+    var routeName = "";
+    var routeDescription = "";
+    var trackPointList = [];
     var resourceList = [];
     var commentList = [];
 
-    return new Route(mapNodeList, resourceList, commentList);
-  };
+    var parsedJson = JSON.parse(routeJson)
 
-  // TODO: Comment if async method makes sense
-  
-  /**
-   * Translates Domain Model Objects into JSON-LD and stores it on the POD.
-   * @param {Route} route Route Domain Model Object representing a route
-   */
-  sendMapInfo = async route => {
-    // Take MapTriplet info and create JSON-LD
+    for(var key in parsedJson) {
+        var value = parsed[key]
+
+        if(key == "name")
+            routeName = value;
+
+        if(key = ="description")
+            routeDescription = value;
+
+        if(key == "points") {
+            for(var latLong in value) {
+                trackPointList.push(new TrackPoint(/*To put, it depends*/, latLong.schema:latitude, latLong.schema:longitude));
+            }
+        }
+
+        if(key == "comments") {
+            for(var comment in value) {
+                commentList.push(new Comment(comment.@id))
+            }
+        }
+
+        if(key == "media") {
+            for(var media in value) {
+                resourceList.push(new Resource(media.@id))
+            }
+        }
+    }
+
+    return new RouteSpec(routeName, routeDescription, mapNodeList, commentList, resourceList);
+};
+
+
+// TODO: Comment if async method makes sense
+export const sendMapInfo = async mapTriplet => {
+
+    // Take Route info and create JSON-LD
+
+
     // Send JSON-LD to underneath layer calling the needed method
-  };
 }
