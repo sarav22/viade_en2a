b35a85a7524d3b66836545e0d44e9c6649f350b7
Sara Vecino García <UO264093@uniovi.es>
2020-03-11 17:19:34 +0100

    Buttons with the friends

--- src/containers/ManageFriends/manageFriends.component.js
+++ src/containers/ManageFriends/manageFriends.component.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import ldflex from '@solid/query-ldflex';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
 import {
-  ManageFriendsWrapper
+  ManageFriendsWrapper,
+  ManageFriendsCard,
+  ButtonFriend,
+  ButtonDropdown
 } from './manageFriends.style';
 
 /**
@@ -9,10 +15,39 @@ import {
  * @param props
  */
 export const ManageFriendsContent = props => {
+  const { webId,friends} = props;
+
+  async function ldflexDeleter(friend){
+     return ldflex[webId].knows.delete(ldflex[friend]);
+  }
+  async function deleteFriend(event, friend) {
+    event.preventDefault();
+    ldflexDeleter(friend);
+    //reload the friends list
+  }
+  
+  async function viewRoutes(event, friend) {
+    event.preventDefault();
+    //redirect to friend routes
+  }  
+
   return (
     <ManageFriendsWrapper data-testid="manageFriends-wrapper">
-    
-       
+      <ManageFriendsCard>
+      {
+        friends.map(friend => (
+          <div><Dropdown as={ButtonGroup}>
+          <ButtonFriend id="t" variant="success"  onClick={(event) => viewRoutes(event,friend)} width='20' >{friend}</ButtonFriend>
+            <Dropdown.Toggle split variant="success" id="dropdown-split-basic" >🠻</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <div><Dropdown.Item href={friend}><ButtonDropdown>View profile</ButtonDropdown></Dropdown.Item></div>
+              <div><Dropdown.Item onClick={(event) => deleteFriend(event,friend)}><ButtonDropdown>Delete</ButtonDropdown></Dropdown.Item></div>
+              <div><Dropdown.Item onClick={(event) => viewRoutes(event,friend)}><ButtonDropdown>View routes</ButtonDropdown></Dropdown.Item></div>
+            </Dropdown.Menu>
+        </Dropdown></div>
+        ))
+      }
+      </ManageFriendsCard>
     </ManageFriendsWrapper>
   );
 };
--- src/containers/ManageFriends/manageFriends.style.js
+++ src/containers/ManageFriends/manageFriends.style.js
@@ -17,6 +17,46 @@ export const ManageFriendsWrapper = styled.section`
   }
 `;
 
+export const ManageFriendsCard = styled.div`
+  background-color: #fff;
+  margin: 30px auto;
+
+  //Overriding the style guide card flexbox settings
+  max-width: 80% !important;
+  flex-direction: row !important;
+  padding: 50px 0 !important; //temporary fix to a style guide bug
+
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  button {
+    margin-left: 0px;
+  }
+`;
+
+export const ButtonFriend = styled.button`
+  width: 400px;
+  display: inline-block;
+  &:first-child {
+    margin-right: 0px;
+  }
+`;
+
+export const ButtonDropdown = styled.button`
+  width: 120px;
+  display: inline-block;
+  &:first-child {
+    margin-right: 10px;
+  }
+`;
+
+
 
 
 
