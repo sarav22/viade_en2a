ac218939bc5fbeaab731b901bfa5b5f10fd6d780
Sara Vecino García <UO264093@uniovi.es>
2020-03-13 12:16:44 +0100

    viewRoutes method redirects to the correct url

--- src/containers/ManageFriends/manageFriends.component.js
+++ src/containers/ManageFriends/manageFriends.component.js
@@ -4,6 +4,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { useTranslation } from 'react-i18next';
+import {browserHistory} from 'react-router';
 import {
   ManageFriendsWrapper,
   ManageFriendsCard,
@@ -34,7 +35,8 @@ export const ManageFriendsContent = props => {
   
   async function viewRoutes(event, friend) {
     event.preventDefault();
-    //redirect to friend routes
+    browserHistory.push('/friendRoutes');
+    await reload();
   }  
 
   return (
@@ -44,7 +46,7 @@ export const ManageFriendsContent = props => {
         friends.map(friend => (
         <Dropdown as={ButtonGroup}>
           <ButtonFriend variant="success"  onClick={(event) => viewRoutes(event,friend)} width='20' data-testid={"buttonFriend"+friend}  key={"buttonFriend"+friend}>{friend}</ButtonFriend>
-          <DropdownButton variant="light" id="dropdown-basic-button" key={friend+"dropdown"} title=""> 
+          <DropdownButton variant="light" key={friend+"dropdown"} title=""> 
         <Dropdown.Item as="button" href={friend} key={friend+"dropdownI1"}>{t('manageFriends.viewProfile')}</Dropdown.Item>
         <Dropdown.Item as="button"  onClick={(event) => deleteFriend(event,friend)} key={friend+"dropdownI2"}>{t('manageFriends.delete')}</Dropdown.Item>
         <Dropdown.Item as="button"  onClick={(event) => viewRoutes(event,friend)} key={friend+"dropdownI3"}>{t('manageFriends.viewRoutes')}</Dropdown.Item>
