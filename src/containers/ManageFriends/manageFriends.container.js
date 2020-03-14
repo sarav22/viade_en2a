21c548f897ffcaa2f773517241b88651495987b9
UO264446 <uo264446@uniovi.es>
2020-03-14 01:45:03 +0100

    Search bar + function to search friends added

--- src/containers/ManageFriends/manageFriends.container.js
+++ src/containers/ManageFriends/manageFriends.container.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
 import { ManageFriendsContent } from './manageFriends.component';
+import {foaf} from 'rdf-namespaces';
+import { fetchDocument } from 'tripledoc';
 
 
 /**
  * Container component for the Welcome Page, containing example of how to fetch data from a POD
  */
 export class ManageFriendsComponent extends Component<Props> {
+	
+	constructor(props){
+    super(props);
+		this.state={
+      friends:null,
+      searchResults:null
+    }
+    this.handleChange = this.handleChange.bind(this);
+	}
  
+   componentDidMount() {
+     this.loadFriends();
+   }
    
-  render() {
-    return (
-      <ManageFriendsContent {...{ }} />
-    );
-  }
+
+    async loadFriends() {
+      const profileDoc =  await fetchDocument(this.props.webId);
+      const profile = profileDoc.getSubject(this.props.webId);
+      const fs=profile.getAllRefs(foaf.knows);
+      this.setState({friends: fs});
+   }
+
+    async searchFriends(matchingString) {
+      const filtered = this.state.friends.filter(f=>f.toLowerCase().includes(matchingString.toLowerCase()));
+      this.setState({searchResults: filtered});
+    }
+
+    handleChange(e) {
+      stringToSearch = e.target.value;
+      if (stringToSearch !== "") {
+        searchFriends(stringToSearch);
+        }
+      }
+    }
+
+
+   render() {
+    if (this.state.friends==null) {
+      return <div/>
+    }else{
+     const friends=this.state.friends;
+     const webId=this.props.webId;
+     const searchResults = this.state.searchResults;
+     return (
+       <div>
+        <ManageFriendsContent {...{ webId, friends}} />
+        <input type="text" className="input" placeholder="Search..." onChange={this.handleChange} />
+        <ManageFriendsContent {...{ webId, searchResults}} />
+      </div>
+     );
+    }
+   }
 }
--- src/containers/ManageFriends/manageFriends.style.js
+++ src/containers/ManageFriends/manageFriends.style.js
@@ -5,7 +5,7 @@ export const ManageFriendsWrapper = styled.section`
   background-image: url('/img/concentric-hex-pattern_2x.png');
   background-repeat: repeat;
   padding: 50px 0;
-
+  flex-direction: row !important;
   h3 {
     color: #666666;
     span {
@@ -17,6 +17,31 @@ export const ManageFriendsWrapper = styled.section`
   }
 `;
 
+export const ManageFriendsCard = styled.div`
+  background-color: #fff;
+  margin: 30px auto;
+  //Overriding the style guide card flexbox settings
+  max-width: 80% !important;
+  flex-direction: row !important;
+  padding: 50px 0 !important; //temporary fix to a style guide bug
+  align-items: center;
+  a {
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  button {
+    margin-left: 0px;
+  }
+`;
 
-
-
+export const ButtonFriend = styled.button`
+  width: 400px;
+  display: inline-block;
+  margin-left: 10px;
+  flex-direction: row !important;
+  &:first-child {
+    margin-right: 0px;
+  }
+`;
