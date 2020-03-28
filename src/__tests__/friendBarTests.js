afe0a18d47dbe81fefd52815872d0848e1ac5412
Raúl Pérez Molinero <UO263743@uniovi.es>
2020-03-28 12:31:47 +0100

    Designed acceptance tests

--- src/__tests__/friendBarTests.js
+++ src/__tests__/friendBarTests.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, wait } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
 import FriendBar from "@containers/FriendRoutes/FriendBar/friendBar.component";
 import '@testing-library/jest-dom/';
 
