04f4ee726695c13d68355b9edb07d1853555e5f8
Ángel Olmedo García <angelixus99@gmail.com>
2020-04-12 21:25:07 +0200

    Deleted commented lines

--- src/__tests__/importViewTests.js
+++ src/__tests__/importViewTests.js
@@ -12,21 +12,4 @@ const { container, getByTestId } = render(
 test('Renders correctly', () => {
     expect(container).toBeTruthy();
     expect(getByTestId('dropMenu')).toBeTruthy();
-})
-
-//test('Renders component', () => {
-  //  expect(container).toBeTruthy();
-    //expect(getByTestId('friendBar-component')).toBeTruthy();
-    //The timeout is neccesary in order to let the app load the data from the POD
-    //setTimeout(() => { 
-    //    expect(getByTestId('friendBarName').textContent).toBe('Luis Presa Collada'); 
-    //}, 5000);
-//});
-
-//test('Renders back button', () =>{
- //   expect(getByTestId('friend-backButton')).toBeTruthy();
-//});
-
-//test('Renders dropdown', () =>{
- //   expect(getByTestId('friend-dropdown')).toBeTruthy();
-//});
+});
