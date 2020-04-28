ac9d6c2e384b7dbcd7a0788ba2442fb259b638cb
Raúl Pérez Molinero <UO263743@uniovi.es>
2020-04-28 17:03:14 +0200

    Styled labels

--- src/containers/Map/LateralMenu/children/MultimediaTab/multimediaTab.component.js
+++ src/containers/Map/LateralMenu/children/MultimediaTab/multimediaTab.component.js
@@ -9,6 +9,8 @@ import VinculationForm from './VinculationForm/vinculationForm.component'
 import ReactPlayer from 'react-player';
 import ReactAudioPlayer from 'react-audio-player';
 import { useTranslation } from 'react-i18next';
+import { MediaListWrapper } from "./multimediaTab.style";
+
 const MultimediaTab = props => {
 
     const { t } = useTranslation();
@@ -26,49 +28,50 @@ const MultimediaTab = props => {
                     <Row>
                         <VinculationForm routeObject = {routeObject} />
                     </Row>
-                    <p>{t("mapView.resources.pictures")} </p>
-                    {routeObject.imagesToDisplay.map((item) => {
-                        return (
-                            <Row>
-                                <Col>
-                                    <Image src={item.resourceUrl}
-                                        style={{ "padding": "15px", "max-width":"300px", "max-height":"300px" }}
-                                    />
-                                </Col>
+                    <MediaListWrapper>
+                        <p className="label">{t("mapView.resources.pictures")} </p>
+                        {routeObject.imagesToDisplay.map((item) => {
+                            return (
+                                <Row>
+                                    <Col>
+                                        <Image src={item.resourceUrl}
+                                            style={{ "padding": "15px", "max-width":"300px", "max-height":"300px" }}
+                                        />
+                                    </Col>
 
-                            </Row>
+                                </Row>
 
-                        );
-                    }
-                    )}
+                            );
+                        }
+                        )}
 
-                    <p>{t("mapView.resources.videos")} </p>
-                    {routeObject.videosToDisplay.map((item) => {
-                        return (
-                            <Row>
-                                <Col>
-                                    <ReactPlayer url={item.resourceUrl} controls />
-                                </Col>
+                        <p className="label">{t("mapView.resources.videos")} </p>
+                        {routeObject.videosToDisplay.map((item) => {
+                            return (
+                                <Row>
+                                    <Col>
+                                        <ReactPlayer url={item.resourceUrl} controls />
+                                    </Col>
 
-                            </Row>
+                                </Row>
 
-                        );
-                    }
-                    )}
-                    <p>{t("mapView.resources.audios")} </p>
-                    {routeObject.audiosToDisplay.map((item) => {
-                        return (
-                            <Row>
-                                <Col>
-                                    <ReactAudioPlayer src={item.resourceUrl} controls/>
-                                </Col>
+                            );
+                        }
+                        )}
+                        <p className="label">{t("mapView.resources.audios")} </p>
+                        {routeObject.audiosToDisplay.map((item) => {
+                            return (
+                                <Row>
+                                    <Col>
+                                        <ReactAudioPlayer src={item.resourceUrl} controls/>
+                                    </Col>
 
-                            </Row>
+                                </Row>
 
-                        );
-                    }
-                    )}
-
+                            );
+                        }
+                        )}
+                    </MediaListWrapper>
                 </Container>
             </Modal.Body>
         </Modal.Dialog>
