6bde6605782666d6bbe8d010ee5c643aeb718a0e
Raúl Pérez Molinero <UO263743@uniovi.es>
2020-04-30 15:39:52 +0200

    Starting to fix e2e tests

--- e2e/step_definitions/viewFriendRoutes.steps.js
+++ e2e/step_definitions/viewFriendRoutes.steps.js
@@ -1,25 +1,33 @@
-/*const { defineFeature, loadFeature } = require('jest-cucumber');
+/*import { setDefaultOptions } from 'expect-puppeteer';
+
+const { defineFeature, loadFeature } = require('jest-cucumber');
 const feature = loadFeature('./e2e/features/viewFriendRoutes.feature');
+
+setDefaultOptions({ timeout: 10000 })
 
 defineFeature(feature, test => {
     beforeEach(async () => {
-        await page.goto('http://localhost:3000');
-        let alice = "https://hayquecrearunpod.inrupt.net/profile/card#me";
-        await expect(page).toFill('input[name="idp"]', alice )
-        //submit
-        await page.evaluate(()=>document.querySelector('#root > div.sc-hrWEMg.dyHRcO > div.sc-bdVaJa.jOKaIj.sc-bsbRJL.eGaoon > section > div > div > div > div > form > button.sc-gzVnrw.isbeaB.ids-link').click())
+        await page.goto('https://arquisoft.github.io/viade_en2a/');
+        /*
         await expect(page).toFill('input[name="username"]', alice )
         await expect(page).toFill('input[name="password"]', "hayquecrearunpod" )
-        await expect(page).toClick('button', { name: 'login' })
+        await expect(page).toClick('button', { name: 'login' })*/
     });
 
     test("Listing the routes created by a friend", ({given, when, then}) => {
         given("I click on the 'Manage Friends' navbar option", async () => {
-            await expect(page).toClick('button', {id: 'manageFriends'});
+            //await expect(page).toClick('button', {id: 'manageFriends'});
+            let alice = "https://en2aviade.inrupt.net/profile/card#me";
+            await expect(page).toMatchElement('input', {name: 'idp'})
+            await expect(page).toFill('input[name="idp"]', alice )
+            await expect(page).toMatchElement('button', {type: 'submit'})
+            await expect(page).toClick('button', {text: 'Log In'})
+            await expect(page).toMatchElement('button', {text: 'Log In'})
+            //submit
         });
 
         when("I click on one friend", async () => {
-            await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
+            //await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
         });
 
         then("A window with the name of that friend and a list of all his shared routes should appear", async () => {
@@ -29,12 +37,12 @@ defineFeature(feature, test => {
 
     test("Viewing a shared route", ({given, when, then}) => {
         given("I already clicked on one friend", async () => {
-            await expect(page).toClick('button', {id: 'manageFriends'});
-            await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
+            //await expect(page).toClick('button', {id: 'manageFriends'});
+            //await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
         });
 
         when("I click on a route", async () => {
-            await expect(page).toClick('button', { text: 'Test Route' })
+            //await expect(page).toClick('button', { text: 'Test Route' })
         });
 
         then("I see it displayed on the map", async () => {
@@ -44,12 +52,12 @@ defineFeature(feature, test => {
 
     test("Clicking on the wrong friend", ({given, when, then}) => {
         given("I already clicked on one friend", async () => {
-            await expect(page).toClick('button', {id: 'manageFriends'});
-            await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
+            //await expect(page).toClick('button', {id: 'manageFriends'});
+            //await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
         });
 
         when("I click the 'Back' button", async () => {
-            await expect(page).toClick('button', { id: 'back' })
+            //await expect(page).toClick('button', { id: 'back' })
         });
 
         then("The 'Manage Friends' view should be shown", async () => {
@@ -59,8 +67,8 @@ defineFeature(feature, test => {
 
     test("Viewing the profile of a friend", ({given, when, then}) => {
         given("I already clicked on one friend", async () => {
-            await expect(page).toClick('button', {id: 'manageFriends'});
-            await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
+            //await expect(page).toClick('button', {id: 'manageFriends'});
+            //await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
         });
 
         when("I click on the dropdown menu and select 'View Profile'", async () => {
@@ -74,8 +82,8 @@ defineFeature(feature, test => {
 
     test("Deleting a friend", ({given, when, then}) => {
         given("I already clicked on one friend", async () => {
-            await expect(page).toClick('button', {id: 'manageFriends'});
-            await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
+            //await expect(page).toClick('button', {id: 'manageFriends'});
+            //await expect(page).toClick('button', { text: 'https://raulpemol.inrupt.net/profile/card#me' })
         });
 
         when("I click on the dropdown menu and select 'Delete Friend'", async () => {
@@ -86,4 +94,5 @@ defineFeature(feature, test => {
 
         });
     });
-});*/
+});
+*/
